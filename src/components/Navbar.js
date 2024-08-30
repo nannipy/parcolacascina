f3f8a7e7daee464433546fc1b60e52f12aa770ce
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-08-30 18:14:01 +0200

    hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-white text-yellow-400 p-4 sticky top-0 z-10">
       <div className="container mx-auto flex justify-between items-center">
         <a href="/">
-        <div className="flex items-center">
-          <img src="/logo.png" alt="Parco La Cascina Logo" className="h-16 mr-8" />
-        </div>
+          <div className="flex items-center">
+            <img src="/logo.png" alt="Parco La Cascina Logo" className="h-16 mr-8" />
+          </div>
         </a>
-        <ul className="flex space-x-4">
+
+        {/* Hamburger Icon */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="focus:outline-none">
+            <div className={`w-6 h-6 relative transform transition-transform duration-300 ${isOpen ? 'rotate-45' : ''}`}>
+              {/* Top line */}
+              <span
+                className={`block absolute h-0.5 w-full bg-yellow-500 transform transition-transform duration-300 ${isOpen ? 'rotate-180 ' : '-translate-y-1.5'}`}
+              ></span>
+              {/* Middle line */}
+              <span
+                className={`block absolute h-0.5 w-full bg-yellow-500 transform transition-opacity duration-300 ${isOpen ? 'opacity-0' : 'opacity-100'}`}
+              ></span>
+              {/* Bottom line */}
+              <span
+                className={`block absolute h-0.5 w-full bg-yellow-500 transform transition-transform duration-300 ${isOpen ? '-rotate-90 ' : 'translate-y-1.5'}`}
+              ></span>
+            </div>
+          </button>
+        </div>
+
+        {/* Menu Links for Desktop */}
+        <ul className="hidden md:flex space-x-4">
           <li><Link to="/" className="hover:text-orange-300 transition-colors font-semibold text-yellow-500">Prodotti</Link></li>
           <li><Link to="/about" className="hover:text-orange-300 transition-colors font-semibold text-yellow-500">Chi Siamo</Link></li>
           <li><Link to="/contact" className="hover:text-orange-300 transition-colors font-semibold text-yellow-500">Contatti</Link></li>
         </ul>
       </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <ul className="md:hidden mt-4 space-y-2">
+          <li><Link to="/" className="block text-yellow-500 hover:text-orange-300 transition-colors font-semibold">Prodotti</Link></li>
+          <li><Link to="/about" className="block text-yellow-500 hover:text-orange-300 transition-colors font-semibold">Chi Siamo</Link></li>
+          <li><Link to="/contact" className="block text-yellow-500 hover:text-orange-300 transition-colors font-semibold">Contatti</Link></li>
+        </ul>
+      )}
     </nav>
   );
 };
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -17,29 +17,31 @@ const ContactPage = () => {
             <ul className="space-y-4">
               <li className="flex items-center">
                 <FaPhoneAlt className="mr-2 text-yellow-500" />
-                <a href="tel:+393426623721" className="hover:text-yellow-500 text-green-700 transition-colors">+39 342 662 3721</a>
+                <a href="tel:+393426623721" className="hover:text-yellow-500 text-green-800 transition-colors">+39 342 662 3721</a>
               </li>
               <li className="flex items-center">
                 <FaEnvelope className="mr-2 text-yellow-500" />
-                <a href="mailto:parcolacascina@gmail.com" className="hover:text-yellow-500 text-green-700  transition-colors">parcolacascina@gmail.com</a>
+                <a href="mailto:parcolacascina@gmail.com" className="hover:text-yellow-500 text-green-800  transition-colors">parcolacascina@gmail.com</a>
               </li>
               <li className="flex items-center">
                 <FaMapMarkerAlt className="mr-2 text-yellow-500 " />
-                <p className='text-green-700 '> Via dei Canneti, 1, 21020 Bodio Lomnago VA</p>
+                <p className='text-green-800 '> Via dei Canneti, 1, 21020 Bodio Lomnago VA</p>
               </li>
             </ul>
           </div>
+          
+
           <div>
             <h2 className="text-2xl font-semibold mb-4 text-green-800">Orari di Apertura</h2>
-            <p className="mb-2 text-green-700 ">Lunedì - Venerdì: 9:00 - 17:00</p>
-            <p className="mb-2 text-green-700 ">Sabato: Chiuso</p>
-            <p className="text-green-700">Domenica: Chiuso</p>
+            <p className="mb-2 text-green-800 ">Lunedì - Venerdì: 9:00 - 18:00</p>
+            <p className="mb-2 text-green-800 ">Sabato: 9:00 - 13:00</p>
+            <p className="text-green-800">Domenica: Chiuso</p>
           </div>
         </div>
       </div>
       <div className="bg-white rounded-lg shadow-lg p-6 overflow-hidden">
         <h2 className="text-2xl font-semibold mb-4 text-green-800">La nostra posizione</h2>
-        <div className="relative w-full h-0 pb-[35%]">
+        <div className="relative w-full h-0 pb-[40%]">
           <iframe 
             title="Parco La Cascina Map"
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d695.4608048163985!2d8.7488587!3d45.7943686!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47867c1f1be44223%3A0xe5f5c2833e0db7dd!2sVia%20dei%20Canneti%2C%201%2C%2021020%20Bodio%20Lomnago%20VA!5e0!3m2!1sit!2sit!4v1725029984811!5m2!1sit!2sit" 
