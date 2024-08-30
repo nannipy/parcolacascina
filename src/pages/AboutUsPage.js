8fbe324ebfbfd025a56478646734e2847b135096
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-08-30 18:18:51 +0200

    autoplay no

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -36,7 +36,6 @@ const AboutUsPage = () => {
         src="images/reel_plc.mp4"
         style={{ height: '500px' }}
         controls
-        autoPlay
         loop
         muted
         className="rounded-lg shadow-lg"
